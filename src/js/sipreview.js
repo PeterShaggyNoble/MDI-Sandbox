68a4db7d7ca3767b38a63bfdb728c4750e903933
Peter Noble <petershaggynoble@gmail.com>
2020-04-15 12:53:49 +0100

    Add upload feature to SI preview generator

--- src/js/sipreview.js
+++ src/js/sipreview.js
@@ -2,12 +2,17 @@
 	const 	data=Object.entries(await(await fetch(`https://houseofdesign.ie/data/icons/simpleicons.json`)).json()),
 		meta=(await(await fetch(`https://raw.githubusercontent.com/simple-icons/simple-icons/develop/_data/simple-icons.json`)).json()).icons,
 		count=(await(await fetch(`https://raw.githubusercontent.com/simple-icons/simple-icons/master/_data/simple-icons.json`)).json()).icons.length,
+		buttons={
+			save:document.querySelector(`button:last-of-type`),
+			upload:document.querySelector(`button:first-of-type`)
+		},
 		inputs={
-			data:document.getElementById(`data`),
-			overlay:document.getElementById(`overlay`),
+			action:document.getElementById(`action`),
 			colour:document.getElementById(`colour`),
+			data:document.getElementById(`data`),
 			name:document.getElementById(`name`),
-			action:document.getElementById(`action`)
+			overlay:document.getElementById(`overlay`),
+			upload:document.createElement(`input`)
 		},
 		text={
 			type:document.getElementById(`type`),
@@ -15,6 +20,7 @@
 			brand:document.getElementById(`brand`),
 			color:document.getElementById(`color`)
 		},
+		a=document.createElement(`a`),
 		svg=document.querySelector(`figure>svg`),
 		background=document.getElementById(`background`),
 		path=document.querySelector(`#path>path`),
@@ -24,11 +30,21 @@
 		context=canvas.getContext(`2d`),
 		width=canvas.width,
 		height=canvas.height,
-		button=document.querySelector(`button`),
-		a=document.createElement(`a`),
+		parser=new DOMParser,
+		reader=new FileReader,
 		xml=new XMLSerializer,
 		image=new Image,
 		draw=()=>image.src=URL.createObjectURL(new Blob([xml.serializeToString(svg)],{type:`image/svg+xml;charset=utf-8`})),
+		findcolour=value=>{
+			if(icon=meta.find(o=>sanitise(o.title)===sanitise(value))){
+				inputs.colour.value=icon.hex;
+				inputs.colour.dispatchEvent(new Event(`input`));
+			}
+		},
+		finddata=value=>{
+			if(icon=data.find(([,d])=>d===value))
+				findcolour(icon[0]);
+		},
 		keydown=event=>{
 			document.body.classList.toggle(`ctrl`,event.ctrlKey);
 			if(event.ctrlKey)
@@ -45,11 +61,47 @@
 						break;*/
 					case`s`:
 						event.preventDefault();
-						button.dispatchEvent(new Event(`click`));
+						buttons.save.dispatchEvent(new Event(`click`));
+						keyup();
+						break;
+					case`u`:
+						event.preventDefault();
+						buttons.upload.dispatchEvent(new Event(`click`));
+						keyup();
 						break;
 				}
 		},
-		keyup=event=>document.body.classList.toggle(`ctrl`,event.ctrlKey),
+		keyup=event=>document.body.classList.toggle(`ctrl`,event&&event.ctrlKey),
+		load=event=>{
+			let 	parsed=parser.parseFromString(event.target.result,`image/svg+xml`),
+				title=parsed.querySelector(`title`),
+				d=parsed.querySelector(`path`);
+			if(d){
+				d=d.getAttribute(`d`);
+				if(d){
+					path.setAttribute(`d`,inputs.data.value=d);
+					finddata(d);
+					if(title){
+						title=title.firstChild.nodeValue.replace(/ icon$/,``);
+						if(title){
+							text.file.textContent=sanitise(inputs.name.value=text.brand.textContent=title);
+							if(!icon)
+								findcolour(title);
+						}
+					}
+				}
+				else alert(`No path data detcted`);
+			}else alert(`No path tag detcted`);
+		},
+		read=()=>{
+			let file=inputs.upload.files[0];
+			if(file)
+				if(file.type===`image/svg+xml`)
+					reader.readAsText(inputs.upload.files[0]);
+				else alert(`Invalid file type`);
+			inputs.upload.remove();
+		},
+		sanitise=value=>value.toLowerCase().replace(/\+/g,`plus`).replace(/^\./,`dot-`).replace(/\.$/,`-dot`).replace(/\./g,`-dot-`).replace(/^&/,`and-`).replace(/&$/,`-and`).replace(/&/g,`-and-`).replace(/[ !:’']/g, "").replace(/à|á|â|ã|ä/g,`a`).replace(/ç|č|ć/g,`c`).replace(/è|é|ê|ë/g,`e`).replace(/ì|í|î|ï/g,`i`).replace(/ñ|ň|ń/g,`n`).replace(/ò|ó|ô|õ|ö/g,`o`).replace(/š|ś/g,`s`).replace(/ù|ú|û|ü/g,`u`).replace(/ý|ÿ/g,`y`).replace(/ž|ź/g,`z`),
 		save=()=>{
 			canvas.toBlob(blob=>{
 				a.href=URL.createObjectURL(blob);
@@ -60,8 +112,11 @@
 				URL.revokeObjectURL(a.href);
 			});
 		},
-		sanitise=value=>value.toLowerCase().replace(/\+/g,`plus`).replace(/^\./,`dot-`).replace(/\.$/,`-dot`).replace(/\./g,`-dot-`).replace(/^&/,`and-`).replace(/&$/,`-and`).replace(/&/g,`-and-`).replace(/[ !:’']/g, "").replace(/à|á|â|ã|ä/g,`a`).replace(/ç|č|ć/g,`c`).replace(/è|é|ê|ë/g,`e`).replace(/ì|í|î|ï/g,`i`).replace(/ñ|ň|ń/g,`n`).replace(/ò|ó|ô|õ|ö/g,`o`).replace(/š|ś/g,`s`).replace(/ù|ú|û|ü/g,`u`).replace(/ý|ÿ/g,`y`).replace(/ž|ź/g,`z`),
 		setfill=hex=>parseInt(hex.substr(0,2),16)*.299+parseInt(hex.substr(2,2),16)*.587+parseInt(hex.substr(4,2),16)*.114<160?`#fff`:`#000`,
+		upload=()=>{
+			document.body.append(inputs.upload);
+			inputs.upload.click();
+		},
 		generate=event=>{
 			clearTimeout(timer);
 			timer=setTimeout(()=>{
@@ -69,6 +124,11 @@
 				value=target.value.trim();
 				delay=0;
 				switch(target){
+					case inputs.action:
+						value&&action.setAttribute(`d`,value);
+						action.setAttribute(`fill-opacity`,value?1:0);
+						delay=200;
+						break;
 					case inputs.colour:
 						if(!inputs.overlay.value){
 							if(!target.validity.valid)
@@ -84,13 +144,14 @@
 						break;
 					case inputs.data:
 						path.setAttribute(`d`,value);
-						if(icon=data.find(([,d])=>d===value))
-							if(icon=meta.find(d=>sanitise(d.title)===icon[0])){
-								inputs.name.value=icon.title;
-								setTimeout(()=>inputs.name.dispatchEvent(new Event(`input`)));
-								inputs.colour.value=icon.hex;
-								inputs.colour.dispatchEvent(new Event(`input`));
-							}
+						finddata(value);
+						if(icon){
+							inputs.name.value=icon.title;
+							setTimeout(()=>inputs.name.dispatchEvent(new Event(`input`)));
+						}
+						break;
+					case inputs.name:
+						text.file.textContent=sanitise(text.brand.textContent=value);
 						break;
 					case inputs.overlay:
 						compare.setAttribute(`fill-opacity`,value?`.5`:`0`);
@@ -101,15 +162,6 @@
 							svg.setAttribute(`fill`,setfill(`111111`));
 						}else inputs.colour.dispatchEvent(new Event(`input`));
 						delay=value?0:200;
-						break;
-					case inputs.name:
-						text.file.textContent=sanitise(value);
-						text.brand.textContent=value;
-						break;
-					case inputs.action:
-						value&&action.setAttribute(`d`,value);
-						action.setAttribute(`fill-opacity`,value?1:0);
-						delay=200;
 						break;
 				}
 				timer=setTimeout(()=>{
@@ -126,8 +178,14 @@
 		URL.revokeObjectURL(image.src);
 	});
 	draw();
-	button.addEventListener(`click`,save);
+	buttons.save.addEventListener(`click`,save);
+	buttons.upload.addEventListener(`click`,upload);
+	inputs.upload.accept=`.svg,image/svg+xml`;
+	inputs.upload.classList.add(`ln`,`pa`);
+	inputs.upload.type=`file`;
+	inputs.upload.addEventListener(`change`,read,false);
+	reader.addEventListener(`load`,load,false);
+	document.addEventListener(`input`,generate,true);
 	document.body.addEventListener(`keydown`,keydown);
 	document.body.addEventListener(`keyup`,keyup);
-	document.addEventListener(`input`,generate,true);
 })();
